cd970133a29ddb2181708aeb86254254e1f0b849
Noah Birnel <nbirnel@gmail.com>
2014-05-29 15:59:51 -0700

    add home page
    rails g controller Welcome index
    vim app/views/welcome/index.html.erb
    add root 'welcome#index' to config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+  get 'welcome/index'
+
   resources :notes, only: [:index, :show]
 
   # The priority is based upon order of creation: first created -> highest priority.
