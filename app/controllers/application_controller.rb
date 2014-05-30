d2223d76c67431d0ef534efd12b0332e525a00a7
Noah Birnel <nbirnel@gmail.com>
2014-05-29 18:35:50 -0700

    factor out app_title

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,28 +2,33 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-end
 
-def index
-  # eg "Recipes - Drum Machines"
-  @title = "#{self.controller_name.capitalize} - Drum Machines"
-end
+  def app_title
+    Rails.application.class.parent.to_s.underscore.titleize
+  end
 
-def show
-  # eg @recipe
-  item = eval "@#{self.controller_name.singularize}"
-  # eg "White Bread - Drum Machines"
-  @title = "#{item.name} - Drum Machines"
-end
+  def index
+    # eg "Recipes - Bake Helper"
+    @title = "#{self.controller_name.capitalize} - #{app_title}"
+  end
 
-def new
-  # eg "New Recipe - Drum Machines"
-  @title = "New #{self.controller_name.singularize.capitalize} - Drum Machines"
-end
+  def show
+    # eg @recipe
+    item = eval "@#{self.controller_name.singularize}"
+    # eg "White Bread - Bake Helper"
+    @title = "#{item.name} - #{app_title}"
+  end
 
-def edit
-  # eg @recipe
-  item = eval "@#{self.controller_name.singularize}"
-  # eg "Edit White Bread - Drum Machines"
-  @title = "Edit #{item.name} - Drum Machines"
+  def new
+    # eg "New Recipe - Bake Helper"
+    @title = "New #{self.controller_name.singularize.capitalize} - #{app_title}"
+  end
+
+  def edit
+    # eg @recipe
+    item = eval "@#{self.controller_name.singularize}"
+    # eg "Edit White Bread - Bake Helper"
+    @title = "Edit #{item.name} - #{app_title}"
+  end
+
 end
