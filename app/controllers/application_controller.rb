42b30a9d7c9a49a15cffdf5b889d9903ffc7e006
Noah Birnel <nbirnel@gmail.com>
2014-05-30 12:29:10 -0700

    include ApplicationHelper in Controller for app_title

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  include ApplicationHelper
+
   def index
     # eg "Recipes - Bake Helper"
     @title = "#{self.controller_name.capitalize} - #{app_title}"
