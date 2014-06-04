62c732e481c7bc5af7889d3948bb5cf1034999f4
Noah Birnel <nbirnel@gmail.com>
2014-06-04 14:58:34 -0700

    add users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140603174702) do
+ActiveRecord::Schema.define(version: 20140604210053) do
 
   create_table "manufacturers", force: true do |t|
     t.string   "name"
@@ -38,6 +38,14 @@ ActiveRecord::Schema.define(version: 20140603174702) do
     t.float    "freq"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "show_email"
   end
 
 end
