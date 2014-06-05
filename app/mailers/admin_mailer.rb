8f04ee42a3f2942f27d8057102f3742027dffbcc
Noah Birnel <nbirnel@gmail.com>
2014-06-05 12:10:23 -0700

    add notify to admin_mailer

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,3 +1,10 @@
 class AdminMailer < ActionMailer::Base
   default from: "admin@drum-machines.net"
+
+  def notify_about_new user
+    @user = user
+    to = "admin@drum-machines.net"
+    to = 'nbirnel@gmail.com' if Rails.env.production?
+    mail(to: to, subject: 'New user Signed Up')
+  end
 end
