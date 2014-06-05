b518a0343a958101443bc624a651ed2e26d03909
Noah Birnel <nbirnel@gmail.com>
2014-06-05 13:06:29 -0700

    remove rename email command from migration

--- db/migrate/20140605152652_add_devise_to_users.rb
+++ db/migrate/20140605152652_add_devise_to_users.rb
@@ -1,7 +1,6 @@
 class AddDeviseToUsers < ActiveRecord::Migration
   def self.up
     change_table(:users) do |t|
-      t.rename :email, :old_email
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
